1afe183b850bbaccef5ad16c93ff5ef9ed333ad4
Elena <elena.r@vivifyideas.com>
2018-11-02 17:38:13 +0100

    #LVAJ-11 Excercise 11 done

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var f = function(){
+    console.log("funkcija someVarScriptBefore:" + someVarScriptBefore);
+    //ne vidi
+    //console.log("funkcija someVarScriptAfter:" + someVarScriptAfter);
+    console.log("funkcija someVarFile:" + someVarFile);
+    //ne vidi
+    //console.log("someVarinAnonymusA:" + someVarinAnonymusA);
+    //ne vidi
+    //console.log("someVarinAnonymusB:" + someVarinAnonymusB);
+    //ne vidi
+    //console.log("some let: " + someLetinAnony);
+
+}
---
+++ js/var.js
@@ -0,0 +1,1 @@
+var someVarFile="SomeVarFile";
